16d951838ae9da467a19377ad10ee26605b1e3e0
santhoshm-i95dev <98160113+santhoshm-i95dev@users.noreply.github.com>
2022-07-20 17:51:06 +0530

    magento frontend backend theme creation

---
+++ Backendtheme/registration.php
@@ -0,0 +1,13 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+
+    'I95dev_Backendtheme',
+
+    __DIR__
+
+);
+
+?>
